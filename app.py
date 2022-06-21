b457a011df84d0bd660e8e5b7f6e402e3be5fb6b
pygitdev <pygitdev@gmail.com>
2022-06-21 17:59:06 +0530

    Layout changed

--- app.py
+++ app.py
@@ -8,16 +8,12 @@ col1, col2 = st.columns(2)
 with open("src/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-with col2:
+with col1:
     App = CreatePage()
     App.create_page()
 
-with col1:
+with col2:
     st.text('Live Preview')
     raw_markdown = preview(App)
-    code = raw_markdown
-    st.text('Raw code')
-    st.code(code, language='markdown')
 
-with col2:
-    download_readme(raw_markdown)
+download_readme(raw_markdown)
